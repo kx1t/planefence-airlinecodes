e7eed1001bd5916e2094a628b6de568024da715d
kx1t <15090643+kx1t@users.noreply.github.com>
2021-03-15 18:36:40 -0400

    auto-upload Mon 15 Mar 2021 06:36:40 PM EDT

---
+++ update-service.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+[[ ! -d "/home/pi/git/planefence-airlinecodes" ]] && git clone https://github.com/kx1t/planefence-airlinecodes /home/pi/git/planefence-airlinecodes
+pushd /home/pi/git/planefence-airlinecodes
+git pull --all
+cp -fu airlinecodes.txt /var/www/php
+
+sed 's/^\([A-Z0-9]\{3\}\).*/\1/g' /var/www/php/airline-missed.txt 2>/dev/null | sort -k1,1 -u | awk NF >/tmp/missed
+
+while read -r missed
+do
+	grep "^$missed," /var/www/php/airlinecodes.txt 2>&1 >/dev/null || echo "Airline code \"$missed\", examples: $(grep "^$missed" /var/www/php/airline-missed.txt |xargs), date added: $(date +"%Y-%m-%d %H:%M:%S %Z")" > airlinecodes-unresolved.txt
+done < /tmp/missed
+
+# remove dupes:
+awk -F',' '!seen[$1]++' airlinecodes-unresolved.txt > /tmp/missed
+mv -f /tmp/missed airlinecodes-unresolved.txt
+touch airlinecodes-unresolved.txt
+
+# now write it back to the repo:
+git add -A *
+git commit -m "auto-upload $(date)"
+git push
+
+popd
