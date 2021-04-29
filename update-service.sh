fdb788291237e9110280425b2c06fee16823f2cc
kx1t <kx1t@amsat.org>
2021-04-29 14:49:02 +0000

    auto-upload Thu 29 Apr 2021 02:49:02 PM UTC

--- update-service.sh
+++ update-service.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-[[ ! -d "/home/pi/git/planefence-airlinecodes" ]] && git clone https://github.com/kx1t/planefence-airlinecodes /home/pi/git/planefence-airlinecodes
-pushd /home/pi/git/planefence-airlinecodes
-git pull --all
-cp -fu airlinecodes.txt /var/www/php
+AIRLINENAMESDIR=~/airlinenames
 
-sed 's/^\([A-Z0-9]\{3\}\).*/\1/g' /var/www/php/airline-missed.txt 2>/dev/null | sort -k1,1 -u | awk NF >/tmp/missed
+[[ ! -d ~/git/planefence-airlinecodes ]] && git clone https://github.com/kx1t/planefence-airlinecodes ~/git/planefence-airlinecodes
+pushd ~/git/planefence-airlinecodes
+git pull --all
+cp -fu airlinecodes.txt $AIRLINENAMESDIR
+
+tmpfile=$(mktemp)
+tmpfile2=$(mktemp)
+
+sed 's/^\([A-Z0-9]\{3\}\).*/\1/g' $AIRLINENAMESDIR/airline-missed.txt 2>/dev/null | sort -k1,1 -u | awk NF >$tmpfile
 
 while read -r missed
 do
-	grep "^$missed," /var/www/php/airlinecodes.txt 2>&1 >/dev/null || echo "Airline code \"$missed\", examples: $(grep "^$missed" /var/www/php/airline-missed.txt |xargs)" > airlinecodes-unresolved.txt
-done < /tmp/missed
+	tac $AIRLINENAMESDIR/airline-missed.txt | grep "^$missed," $AIRLINENAMESDIR/airlinecodes.txt 2>&1 >/dev/null || echo "Airline code \"$missed\", examples:$(tac $AIRLINENAMESDIR/airline-missed.txt| grep "^$missed" | xargs)" >> $tmpfile2
+done < $tmpfile
 
 # remove dupes:
-awk -F',' '!seen[$1]++' airlinecodes-unresolved.txt > /tmp/missed
-mv -f /tmp/missed airlinecodes-unresolved.txt
+cat $tmpfile2 airlinecodes-unresolved.txt | awk -F',' '!seen[$1]++' airlinecodes-unresolved.txt > $tmpfile
+mv -f $tmpfile airlinecodes-unresolved.txt
 touch airlinecodes-unresolved.txt
+rm $tmpfile2
+
+# back up the server dir:
+pushd $AIRLINENAMESDIR && tar -czvf backup.tgz ./* && popd && mv $AIRLINENAMESDIR/backup.tgz .
+
 
 # now write it back to the repo:
 git add -A *
